5cd9fbf5f2a4e9bc160eb65af1eb8ef56c682fc7
devis <devis@Devi>
2024-10-01 12:38:34 +0530

    changes commited

---
+++ com.ComcastCRMFramework/src/test/java/com/ComcastCRMFramework/OrgTest/ProductTest.java
@@ -0,0 +1,12 @@
+package com.ComcastCRMFramework.OrgTest;
+
+public class ProductTest {
+
+	public static void main(String[] args) {
+	System.out.println("product is cteated");
+	System.out.println("cse");
+	
+	System.out.println("product is cteated");
+	}
+
+}
